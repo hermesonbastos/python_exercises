059c3fc8f7c2b89a3ba36b952468fcfd9b2c3b10
HBM18 <hermesonbastos@gmail.com>
2022-08-19 21:09:03 -0300

    ex003

--- Python_Mundo_1/ex002.py
+++ Python_Mundo_1/ex002.py
@@ -1,4 +1,4 @@
-# Imprimir uma informação oferecida pelo usuário
+# Imprimir uma informação oferecida pelo usuário 
 
 nome = input('Digite o seu nome: ')
 print('Seja muito bem vindo, é um prazer te conhecer, {}!'.format(nome))
